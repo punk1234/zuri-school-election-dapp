eb86ccb26260692deec3c21e523934546e0a090e
Kc pele <fidekg123@gmail.com>
2022-04-20 13:06:46 +0000

    added page not found

--- front-end/src/components/404NotFound.js
+++ front-end/src/components/404NotFound.js
@@ -11,8 +11,8 @@ export default function NotFound () {
           <img className='img-fluid' src="https://wizardly-khorana-19e375.netlify.app/Scarecrow.png" alt="" />
         </div>
         <div className="col-md-4">
-          <p className="lead display-2">I have bad news for you</p>
-          <p className="unavaliable">
+          <p className=" display-2">I have bad news for you</p>
+          <p className="lead">
             The page you are looking for might be removed or is temporarily
             unavailable
           </p>
